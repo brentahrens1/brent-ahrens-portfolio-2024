d28e33305a40393b88c3e7438ecfb0ba772d2c34
Brent Ahrens <cidida@gmail.com>
2024-06-27 16:31:39 -0700

    move Overlay into its own component

--- src/components/partials/navigation/navbar.tsx
+++ src/components/partials/navigation/navbar.tsx
@@ -1,6 +1,7 @@
 import close from '../../../assets/close-icon.svg';
 import { useEffect, useState } from 'react';
 import { NavLink, Link } from "react-router-dom";
+import Overlay from './overlay';
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
@@ -113,28 +114,7 @@ const Navbar = () => {
           </NavLink>
         </div>
       </div>
-      <div className={`fixed top-0 left-0 w-full h-screen bg-white z-10 ${isOverlayOpen ? 'flex' : 'hidden'}`}>
-        <div className="flex flex-col items-center gap-14 w-full">
-          <div className="flex flex-col justify-center items-center cursor-pointer transition-all w-full h-full">
-            <NavLink
-              to="/services" className='transition-all duration-200 ease-in-out hover:text-nightblue hover:underline py-3 px-10'>
-              <p className="text-2xl">Services</p>
-            </NavLink>
-            <NavLink 
-              to="/about"
-              className='transition-all duration-200 ease-in-out hover:text-nightblue hover:underline py-3'
-            >
-              <p className="text-2xl">About</p>
-            </NavLink>
-            <NavLink
-              to="/contact"
-              className="px-6 py-3 border drop-shadow-button rounded-3xl bg-offwhite cursor-pointer 
-                transition-all duration-200 ease-in-out hover:text-nightblue hover:underline">
-              <p className="text-2xl">Schedule a call</p>
-            </NavLink>
-          </div>
-        </div>
-      </div>
+      <Overlay isOverlayOpen={isOverlayOpen} />
     </nav>
   );
 };
