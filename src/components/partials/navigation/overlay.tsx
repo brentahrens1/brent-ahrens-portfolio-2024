0a33245bcc3544358667c1ab69ebeaef6973e7e7
Brent Ahrens <cidida@gmail.com>
2024-06-27 17:33:17 -0700

    mobile nav overlay animation

--- src/components/partials/navigation/overlay.tsx
+++ src/components/partials/navigation/overlay.tsx
@@ -6,16 +6,16 @@ type OverlayProps = {
 
 const Overlay = ({ isOverlayOpen }: OverlayProps) => {
   return (
-    <div className={`fixed top-0 left-0 w-full h-screen bg-white z-10 ${isOverlayOpen ? 'flex' : 'hidden'}`}>
+    <div className={`flex fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black z-10 rounded-full ${isOverlayOpen ? 'transition-all duration-1000 h-screen w-full scale-150' : 'transition-all w-0 h-0 duration-1000'}`}>
       <div className="flex flex-col items-center gap-14 w-full">
-        <div className="flex flex-col justify-center items-center cursor-pointer transition-all w-full h-full">
+        <div className={`flex flex-col justify-center items-center cursor-pointer transition-all w-full h-full ${isOverlayOpen ? 'transition-all duration-1000 opacity-100 delay-300': 'transition-all duration-1000 opacity-0'}`}>
           <NavLink
-            to="/services" className='transition-all duration-200 ease-in-out hover:text-nightblue hover:underline py-3 px-10'>
+            to="/services" className='text-white transition-all duration-200 ease-in-out hover:text-nightblue hover:underline py-3 px-10'>
             <p className="text-2xl">Services</p>
           </NavLink>
           <NavLink 
             to="/about"
-            className='transition-all duration-200 ease-in-out hover:text-nightblue hover:underline py-3'
+            className='text-white transition-all duration-200 ease-in-out hover:text-nightblue hover:underline py-3'
           >
             <p className="text-2xl">About</p>
           </NavLink>
