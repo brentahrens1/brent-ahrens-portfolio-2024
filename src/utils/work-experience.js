fb767b20a6a93462d084f068a4d75802bdfe5903
Brent Ahrens <cidida@gmail.com>
2024-11-04 12:00:11 -0800

    add new work info and resume

--- src/utils/work-experience.js
+++ src/utils/work-experience.js
@@ -1,31 +1,36 @@
 export const workExperience = [
   {
-    title: "Founder of Studio Wozzie",
-    company: "Studio Wozzie",
-    years: "2018 - Current",
-    description: <p>— Wozzie, Inc is Brent Ahrens Front-end development studio.<br/><br/>
-        — Working with clients across industries to create custom sites that reflect and elevate the user experience and design vision.<br/><br/>
-        — Partnering with brands and designers using Figma and other collaborative tools to transform ideas and designs into intuitive and engaging web experiences<br/><br/>
-        — Collaborating with other engineers using Github/Git Delivering responsive, crossbrowser compatible and SEO- friendly websites.<br/> 
-        <br/>Clients include: Redesign Health, Hugo & Marie (Elyse Walker), Thinkingbox (L'Oreal), Gates Creative, Made by Grizzly, Studio LaRocca
+    title: "Frontend Engineer",
+    company: "Freelance",
+    years: "2019 - Current",
+    description: <p>
+        — Collaborate with clients across diverse industries to craft bespoke websites. Clients include: Hugo & Marie (Elyse Walker), Thinkingbox (L'Oreal), Gates Creative, Made by Grizzly, Studio LaRocca
       </p>
   },
   {
     title: "Software Engineer",
     company: "Working Not Working",
     years: "2021 - 2023",
-    description: <p>— Worked with small team of developers to implement an agile workflow building out full-stack components, including onboarding flows, status updates, profile and project pages.<br /><br/>
-        — Converted complex designs to pixel-perfect web pages Wrote unit tests for all new functionality to ensure all code worked as intended.<br /><br/>
+    description: <p>— Led development for frontend components, including new recruiter platform NTRNL, onboarding flows, status updates, and profile/project pages.<br /><br/>
+        — Implemented unit testing for all new functionality to guarantee the integrity and reliability of the codebase.<br /><br/>
         — Collaborated cross-functionally with internal teams through the entire dev process from project kickoff, design, QA, deployment, and updates
+      </p>
+  },
+  {
+    title: "Frontend Engineer",
+    company: "Redesign Health · Contract",
+    years: "2020 - 2021",
+    description: <p>—  Led frontend development for all new marketing websites for Redesign's portfolio of healthcare startups (valued at $1B cumulative portfolio revenue).<br /><br/>
+        — Leveraged Next.js and headless CMS platforms such as Storyblok and Contentful.<br /><br/>
+        — Engaged in cross-functional collaboration with internal teams throughout the entire development process from project kickoff to design, QA, deployment, and subsequent updates.
       </p>
   },
   {
     title: "Web Developer",
     company: "Atelier Ace",
-    years: "2021 - 2023",
-    description: <p>— Assisted in building and maintaining the Ace Hotel website including all properties + blog.<br /><br/>
-        — Built and managed 13 food and beverage websites, each with unique identity and branding.<br /><br/>
-        — Worked closely with Design and Marketing teams Worked daily with ticketing systems to manage workflow and ensure on-time delivery of all assets
+    years: "2019 - 2020",
+    description: <p>— Built 10 new food & beverage property websites for the Ace Hotel portfolio.<br /><br/>
+        — Created new frontend components and performed consistent maintenance for Ace Hotel main website.<br /><br/>
       </p>
   },
 ]
